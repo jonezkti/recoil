5b6af76b8a41bc967135fb98a90c9ea16e9eb7a0
Jonezkti <74375063+jonezkti@users.noreply.github.com>
2022-01-25 00:14:53 +0200

    Spacing

--- recoil.lua
+++ recoil.lua
@@ -1,10 +1,10 @@
 Recoils = {
 	-- Pistoolit
-	[`WEAPON_PISTOL`] 				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_PISTOL`] 			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_PISTOL_MK2`] 			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_COMBATPISTOL`] 		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_APPISTOL`] 			= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_PISTOL50`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_PISTOL50`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_SNSPISTOL`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_SNSPISTOL_MK2`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_HEAVYPISTOL`] 			= {recoil = 0.00, shake = 0.0},
@@ -13,7 +13,7 @@ Recoils = {
 	[`WEAPON_REVOLVER`] 			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_REVOLVER_MK2`] 		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_DOUBLEACTION`]			= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_STUNGUN`] 				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_STUNGUN`] 			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_FLAREGUN`] 			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_CERAMICPISTOl`] 		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_NAVYREVOLVER`] 		= {recoil = 0.00, shake = 0.0},
@@ -21,12 +21,12 @@ Recoils = {
 
 	-- SMG
 	[`WEAPON_MICROSMG`] 			= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_SMG`] 					= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_SMG_MK2`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SMG`] 				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SMG_MK2`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_ASSAULTSMG`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_COMBATPDW`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_MACHINEPISTOL`]		= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_MINISMG`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MINISMG`]			= {recoil = 0.00, shake = 0.0},
 
 	-- Haulikot
 	[`WEAPON_PUMPSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
@@ -34,7 +34,7 @@ Recoils = {
 	[`WEAPON_SAWNOFFSHOTGUN`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_ASSAULTSHOTGUN`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_BULLPUPSHOTGUN`]		= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_MUSKET`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MUSKET`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_HEAVYSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_DBSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_AUTOSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
@@ -47,7 +47,7 @@ Recoils = {
 	[`WEAPON_CARBINERIFLE_MK2`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_ADVANCEDRIFLE`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_SPECIALCARBINE`]		= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_SPECIALCARBINE_MK2`]	= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SPECIALCARBINE_MK2`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_BULLPUPRIFLE`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_BULLPUPRIFLE_MK2`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_COMPACTRIFLE`]			= {recoil = 0.00, shake = 0.0},
@@ -55,8 +55,8 @@ Recoils = {
 	[`WEAPON_HEAVYRIFLE`]			= {recoil = 0.00, shake = 0.0},
 
 	-- LMG
-	[`WEAPON_MG`]					= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_COMBATMG`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MG`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMBATMG`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_COMBATMG_MK2`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_GUSENBERG`]			= {recoil = 0.00, shake = 0.0},
 
@@ -65,14 +65,14 @@ Recoils = {
 	[`WEAPON_HEAVYSNIPER`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_HEAVYSNIPER_MK2`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_MARKSMANRIFLE`]		= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_MARKSMANRIFLE_MK2`]	= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MARKSMANRIFLE_MK2`]		= {recoil = 0.00, shake = 0.0},
 
 	-- Heavy
-	[`WEAPON_RPG`]					= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_RPG`]				= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_GRENADELAUNCHER`]		= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_MINIGUN`]				= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_FIREWORK`]				= {recoil = 0.00, shake = 0.0},
-	[`WEAPON_RAILGUN`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MINIGUN`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_FIREWORK`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_RAILGUN`]			= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_HOMINGLAUNCHER`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_COMPACTLAUNCHER`]		= {recoil = 0.00, shake = 0.0},
 	[`WEAPON_EMPLAUNCHER`]			= {recoil = 0.00, shake = 0.0},
