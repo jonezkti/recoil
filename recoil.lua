f6c45fb3f063b504ecba3699dcb7de6aa2d029e6
Jonezkti <74375063+jonezkti@users.noreply.github.com>
2022-01-25 00:14:02 +0200

    Add files via upload

---
+++ recoil.lua
@@ -0,0 +1,123 @@
+Recoils = {
+	-- Pistoolit
+	[`WEAPON_PISTOL`] 				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_PISTOL_MK2`] 			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMBATPISTOL`] 		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_APPISTOL`] 			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_PISTOL50`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SNSPISTOL`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SNSPISTOL_MK2`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_HEAVYPISTOL`] 			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_VINTAGEPISTOL`] 		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MARKSMANPISTOL`] 		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_REVOLVER`] 			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_REVOLVER_MK2`] 		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_DOUBLEACTION`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_STUNGUN`] 				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_FLAREGUN`] 			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_CERAMICPISTOl`] 		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_NAVYREVOLVER`] 		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_GADGETPISTOL`] 		= {recoil = 0.00, shake = 0.0},
+
+	-- SMG
+	[`WEAPON_MICROSMG`] 			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SMG`] 					= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SMG_MK2`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_ASSAULTSMG`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMBATPDW`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MACHINEPISTOL`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MINISMG`]				= {recoil = 0.00, shake = 0.0},
+
+	-- Haulikot
+	[`WEAPON_PUMPSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_PUMPSHOTGUN_MK2`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SAWNOFFSHOTGUN`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_ASSAULTSHOTGUN`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_BULLPUPSHOTGUN`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MUSKET`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_HEAVYSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_DBSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_AUTOSHOTGUN`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMBATSHOTGUN`]		= {recoil = 0.00, shake = 0.0},
+
+	-- AR
+	[`WEAPON_ASSAULTRIFLE`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_ASSAULTRIFLE_MK2`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_CARBINERIFLE`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_CARBINERIFLE_MK2`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_ADVANCEDRIFLE`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SPECIALCARBINE`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_SPECIALCARBINE_MK2`]	= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_BULLPUPRIFLE`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_BULLPUPRIFLE_MK2`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMPACTRIFLE`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MILITARYRIFLE`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_HEAVYRIFLE`]			= {recoil = 0.00, shake = 0.0},
+
+	-- LMG
+	[`WEAPON_MG`]					= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMBATMG`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMBATMG_MK2`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_GUSENBERG`]			= {recoil = 0.00, shake = 0.0},
+
+	-- Sniper
+	[`WEAPON_SNIPERRIFLE`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_HEAVYSNIPER`]			= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_HEAVYSNIPER_MK2`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MARKSMANRIFLE`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MARKSMANRIFLE_MK2`]	= {recoil = 0.00, shake = 0.0},
+
+	-- Heavy
+	[`WEAPON_RPG`]					= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_GRENADELAUNCHER`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_MINIGUN`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_FIREWORK`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_RAILGUN`]				= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_HOMINGLAUNCHER`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_COMPACTLAUNCHER`]		= {recoil = 0.00, shake = 0.0},
+	[`WEAPON_EMPLAUNCHER`]			= {recoil = 0.00, shake = 0.0},
+}
+
+CreateThread(function()
+	while true do
+		Wait(0)
+		local sleep = 500
+		local ped = PlayerPedId()
+		if IsPedArmed(ped, 7) then
+			local currentWeapon = GetSelectedPedWeapon(ped)
+			sleep = 4
+			if IsPedShooting(ped) and not IsPedDoingDriveby(ped) then
+				sleep = 1
+				if Recoils[currentWeapon] then
+					if Recoils[currentWeapon].shake ~= 0 then
+						ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', Recoils[currentWeapon].shake)
+					end
+					if Recoils[currentWeapon].recoil ~= 0 then
+						tv = 0
+						if GetFollowPedCamViewMode() ~= 4 then
+							repeat
+								Wait(0)
+								p = GetGameplayCamRelativePitch()
+								SetGameplayCamRelativePitch(p+0.1, 0.2)
+								tv = tv+0.1
+							until tv >= Recoils[currentWeapon].recoil
+						else
+							repeat
+								Wait(0)
+								p = GetGameplayCamRelativePitch()
+								if Recoils[currentWeapon].recoil > 0.1 then
+									SetGameplayCamRelativePitch(p+0.6, 1.2)
+									tv = tv+0.6
+								else
+									SetGameplayCamRelativePitch(p+0.016, 0.333)
+									tv = tv+0.1
+								end
+							until tv >= Recoils[currentWeapon].recoil
+						end
+					end
+				end
+			end
+		end
+		Wait(sleep)
+	end
+end)
